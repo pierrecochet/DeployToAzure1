1fbb1f225631febeff4a3424f7dc5f901d07255a
cochet <pierre.cochet>
2018-03-15 04:30:12 +0200

    oui

--- app_server/controllers/ctrlSimpleStuff.js
+++ app_server/controllers/ctrlSimpleStuff.js
@@ -1,9 +1,9 @@
 const simplestuff = function(req, res) {
     res.render('simplestuff',{
         reasons:[
-            "Responsive",
-            "Contain a lot of implementation",
-            "It's really good-looking"
+            {reason:"Responsive",advantage:"cool"},
+            {reason:"Contain a lot of implementation",advantage:"faster"},
+            {reason:"It's really good-looking",advantage:"good front-end"}
         ]});
 };
 
---
+++ app_server/scripts/addList.js
@@ -0,0 +1,9 @@
+db.dropDatabase();
+
+db.reasons.save(
+  [
+      {reason:"Responsive",advantage:"cool"},
+      {reason:"Contain a lot of implementation",advantage:"faster"},
+      {reason:"It's really good-looking",advantage:"good front-end"}
+  ]
+);
