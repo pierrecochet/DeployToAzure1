f677e706da3e5fb86459dafd162492d8352ab21c
cochet <pierre.cochet>
2018-04-07 15:37:04 +0300

    moui

--- app_server/controllers/ctrlSimpleStuff.js
+++ app_server/controllers/ctrlSimpleStuff.js
@@ -1,12 +1,34 @@
-const simplestuff = function(req, res) {
-    res.render('simplestuff',{
-        reasons:[
-            {reason:"Responsive",advantage:"cool"},
-            {reason:"Contain a lot of implementation",advantage:"faster"},
-            {reason:"It's really good-looking",advantage:"good front-end"}
-        ]});
-};
+const request = require('request');
+const apiURL = require("./api_URLs");
 
+const simplestuff = function(req,res){
+    const path = '/api/reasons';
+    const requestOptions = {
+        url: apiURL.server+path,
+        method : 'GET',
+        json : {},
+        qs : {}
+    };
+    request (
+        requestOptions,
+        function (err, response, body){
+            if (err){
+                res.render('error', {message: err.message});
+            } else if (response.statusCode !== 200){
+                res.render('error', {message: 'Error accessing API: ' +
+                    response.statusMessage +
+                    ' ('+ response.statusCode + ')' });
+            } else if (!(body instanceof Array)) {
+                res.render('error', {message: 'Unexpected response data'});
+            } else if (!body.length){
+                res.render('error', {message: 'No documents in collection'});
+            } else {
+                res.render('simplestuff', {reasons: body});
+            }
+        }
+    );
+};
 module.exports = {
     simplestuff
-};
+}
+
